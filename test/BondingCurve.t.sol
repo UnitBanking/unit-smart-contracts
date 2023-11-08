91d217c3746397a51954b12646781ec2921232cf
StarDumpling <148853647+StarDumpling@users.noreply.github.com>
2023-11-08 13:31:25 +0100

    Add  `updateInternals` and  more`getInternalPriceForTimestamp` tests

--- test/BondingCurve.t.sol
+++ test/BondingCurve.t.sol
@@ -14,7 +14,7 @@ contract BondingCurveTestTest is Test {
     EthUsdOracle public ethUsdOracle;
     ERC20 public unitToken;
     BondingCurveTest public bondingCurve;
-    
+
     uint256 private constant START_TIMESTAMP = 1699023595;
 
     function setUp() public {
@@ -28,21 +28,97 @@ contract BondingCurveTestTest is Test {
         bondingCurve = new BondingCurveTest(unitToken, inflationOracle, ethUsdOracle);
     }
 
-    function test_getInternalPriceForTimestamp_10days() public {
+    function test_getInternalPriceForTimestamp_10Days() public {
+        // Arrange
         uint256 currentTimestamp = START_TIMESTAMP + 10 days;
         vm.warp(currentTimestamp);
+
+        // Act
         uint256 price = bondingCurve.testGetInternalPriceForTimestamp(block.timestamp);
+
+        // Assert
         uint256 expectedPrice = 1000619095670254662; // 1.000619095670254662
         assertEq(price, expectedPrice);
     }
 
-    function test_getInternalPriceForTimestamp_1month() public {
-        assertEq(START_TIMESTAMP, block.timestamp);
+    function test_getInternalPriceForTimestamp_1Month() public {
+        // Arrange
+        uint256 currentTimestamp = START_TIMESTAMP + 30 days;
+        vm.warp(currentTimestamp);
+
+        // Act
+        uint256 price = bondingCurve.testGetInternalPriceForTimestamp(block.timestamp);
+
+        // Assert
+        uint256 expectedPrice = 1001858437086397421; // 1.001858437086397421
+        assertEq(price, expectedPrice);
     }
 
-    function test_getInternalPriceForTimestamp_0days() public {
+    function test_getInternalPriceForTimestamp_1MonthAnd1Day() public {
+        // Arrange
+        uint256 currentTimestamp = START_TIMESTAMP + 30 days;
+        vm.warp(currentTimestamp);
+        inflationOracle.setPriceIndexTwentyYearsAgo(78);
+        inflationOracle.setLatestPriceIndex(122);
+        bondingCurve.updateInternals();
+
+        vm.warp(currentTimestamp + 1 days);
+
+        // Act
+        uint256 price = bondingCurve.testGetInternalPriceForTimestamp(block.timestamp);
+
+        // Assert
+        // 1.001858437086397421 * 1.000061262150421502
+        uint256 expectedPrice = 1001919813088671258; // 1.001919813088671258
+        assertEq(price, expectedPrice);
+    }
+
+    function test_getInternalPriceForTimestamp_0Days() public {
+        // Arrange & Act
         uint256 price = bondingCurve.testGetInternalPriceForTimestamp(START_TIMESTAMP);
+
+        // Assert
         uint256 expectedPrice = 1e18;
         assertEq(price, expectedPrice);
+    }
+
+    function test_updateInternals() public {
+        // Arrange
+        uint256 lastInternalPriceBefore = bondingCurve.getInternalPrice();
+        uint256 currentTimestamp = START_TIMESTAMP + 30 days;
+        vm.warp(currentTimestamp);
+        uint256 lastOracleUpdateTimestampBefore = bondingCurve.lastOracleUpdateTimestamp();
+        inflationOracle.setPriceIndexTwentyYearsAgo(78);
+        inflationOracle.setLatestPriceIndex(122);
+
+        // Act
+        bondingCurve.updateInternals();
+
+        // Assert
+        uint256 lastOracleUpdateTimestampAfter = bondingCurve.lastOracleUpdateTimestamp();
+        uint256 lastInternalPriceAfter = bondingCurve.getInternalPrice();
+        assertEq(lastInternalPriceBefore, 1e18); // 1
+        assertEq(lastInternalPriceAfter, 1001858437086397421); // 1.001858437086397421
+        assertEq(bondingCurve.lastOracleInflationRate(), 2236);
+        assertGt(lastOracleUpdateTimestampAfter, lastOracleUpdateTimestampBefore);
+    }
+
+    function test_Mint() public {
+        // Arrange
+        address user = vm.addr(1);
+        uint256 etherValue = 1 ether;
+        uint256 userEthBalance = 100 ether;
+        vm.deal(user, userEthBalance);
+        vm.warp(START_TIMESTAMP + 10 days);
+
+        // Act
+        vm.prank(user);
+        bondingCurve.mint{ value: etherValue }(user);
+
+        // Assert
+        assertEq(user.balance, userEthBalance - etherValue);
+        assertEq(unitToken.balanceOf(user), 1e18);
+        console2.log('user.balance', user.balance);
+        console2.log('unitToken.balanceOf(user)', unitToken.balanceOf(user));
     }
 }
