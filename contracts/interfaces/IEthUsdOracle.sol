46d976d75cf51fce2ba35d76bbef614942adf460
aristotleee <148852099+aristotleee@users.noreply.github.com>
2023-11-17 02:56:00 +0800

    Remove mock contracts

--- contracts/interfaces/IEthUsdOracle.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-// Deployed with donations via Gitcoin GR9
-
-pragma solidity 0.8.21;
-
-interface IEthUsdOracle {
-    function getEthUsdPrice() external pure returns (uint256);
-}
--- contracts/interfaces/IInflationOracle.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-// Deployed with donations via Gitcoin GR9
-
-pragma solidity 0.8.21;
-
-interface IInflationOracle {
-    function getInflationRate() external pure returns (uint256);
-}
