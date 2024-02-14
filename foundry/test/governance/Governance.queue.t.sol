997d1ca9a3d3b24a45d86d519aea7315d0b0ed9a
StarDumpling <148853647+StarDumpling@users.noreply.github.com>
2024-02-14 11:43:22 +0100

    Start on governance `queue` tests

---
+++ foundry/test/governance/Governance.queue.t.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.23;
+
+import { GovernanceTestBase } from './GovernanceTestBase.t.sol';
+import { IGovernance } from '../../../contracts/interfaces/IGovernance.sol';
+
+contract GovernanceQueueTest is GovernanceTestBase {
+    function test_queue_RevertsWhenInvalidProposalState() public {
+        // Arrange
+        governanceProxy.setWhitelistAccountExpiration(wallet, block.timestamp + 10);
+        uint256 proposalId = _propose(wallet);
+
+        // Act & Assert
+        IGovernance.ProposalState state = governanceProxy.getState(proposalId);
+        vm.expectRevert(abi.encodeWithSelector(IGovernance.GovernanceInvalidProposalState.selector, IGovernance.ProposalState.Succeeded, state));
+        governanceProxy.queue(proposalId);
+    }
+
+    function _propose(address proposer) private returns (uint256 proposalId) {
+        address[] memory targets = new address[](1);
+        uint256[] memory values = new uint256[](1);
+        string[] memory signatures = new string[](1);
+        bytes[] memory calldatas = new bytes[](1);
+        string memory description = 'proposal #1';
+
+        vm.prank(proposer);
+        governanceProxy.propose(targets, values, signatures, calldatas, description);
+
+        proposalId = governanceProxy.proposalCount();
+    }
+}
