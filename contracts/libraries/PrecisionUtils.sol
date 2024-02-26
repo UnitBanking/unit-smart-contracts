a95b199cfe988eed49ec021326a145380c43312b
franklynot <148856869+franklynot@users.noreply.github.com>
2024-02-26 01:37:33 +0100

    Optimize precision helper functions

--- contracts/libraries/PrecisionUtils.sol
+++ contracts/libraries/PrecisionUtils.sol
@@ -9,37 +9,33 @@ library PrecisionUtils {
         uint256 standardPrecision,
         uint256 collateralTokenDecimals
     ) internal pure returns (uint256 collateralPrecision) {
-        if (collateralTokenDecimals < ProtocolConstants.STANDARD_DECIMALS) {
+        if (collateralTokenDecimals == ProtocolConstants.STANDARD_DECIMALS) {
+            collateralPrecision = standardPrecision;
+        } else if (collateralTokenDecimals < ProtocolConstants.STANDARD_DECIMALS) {
             collateralPrecision =
                 standardPrecision /
                 10 ** (ProtocolConstants.STANDARD_DECIMALS - collateralTokenDecimals);
-        }
-
-        if (collateralTokenDecimals > ProtocolConstants.STANDARD_DECIMALS) {
+        } else {
             collateralPrecision =
                 standardPrecision *
                 10 ** (collateralTokenDecimals - ProtocolConstants.STANDARD_DECIMALS);
         }
-
-        collateralPrecision = standardPrecision;
     }
 
     function toStandardPrecision(
         uint256 collateralPrecision,
         uint256 collateralTokenDecimals
     ) internal pure returns (uint256 standardPrecision) {
-        if (collateralTokenDecimals < ProtocolConstants.STANDARD_DECIMALS) {
+        if (collateralTokenDecimals == ProtocolConstants.STANDARD_DECIMALS) {
+            standardPrecision = collateralPrecision;
+        } else if (collateralTokenDecimals < ProtocolConstants.STANDARD_DECIMALS) {
             standardPrecision =
                 collateralPrecision *
                 10 ** (ProtocolConstants.STANDARD_DECIMALS - collateralTokenDecimals);
-        }
-
-        if (collateralTokenDecimals > ProtocolConstants.STANDARD_DECIMALS) {
+        } else {
             standardPrecision =
                 collateralPrecision /
                 10 ** (collateralTokenDecimals - ProtocolConstants.STANDARD_DECIMALS);
         }
-
-        standardPrecision = collateralPrecision;
     }
 }
